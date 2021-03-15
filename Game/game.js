0e5e13b32d8a9bd80114fb5667eb0e911a3e7a47
JoranFloor <80403870+JoranFloor@users.noreply.github.com>
2021-03-15 01:58:07 +0100

    lees ff
    in de mainscene.js file kan je nu de create preload en update functie gebruiken die staan dus niet meer in de game,js de ze manier werkt beter als je meerdere scenes wilt hebben

--- Game/game.js
+++ Game/game.js
@@ -1,29 +1,17 @@
 // const phaserGame = document.createElement('script');
 // phaserGame.setAttribute('src','//cdn.jsdelivr.net/npm/phaser@3.53.1/dist/phaser.js');
 // document.head.appendChild(phaserGame);
-
-let codey;
-
-function preload() {
-    this.load.image('codey', 'https://content.codecademy.com/courses/learn-phaser/codey.png');
-}
-
-function create() {
-    this.add.text(50, 50, 'start menu');
-    codey = this.add.sprite(30, 200, 'codey');
-}
-
-const config = {
+var game;
+window.onload=function()
+{
+var config = {
     type: Phaser.AUTO,
     width: 1600,
     height: 800,
     backgroundColor: "#5ce835",
-    scene: {
-        create,
-        preload,
-
-    },
-
+    parent: 'phaser-game',
+    scene: [mainScene]
 }
 
-const game = new Phaser.Game(config);
+game = new Phaser.Game(config);
+}
--- Game/mainScene.js
+++ Game/mainScene.js
@@ -1,15 +1,20 @@
+let codey;
 class mainScene extends Phaser.Scene {
+    
     constructor (){
         super('mainScene');
     }
+    
     preload()
     {
     // load images or sounds
+    this.load.image('codey', 'https://content.codecademy.com/courses/learn-phaser/codey.png');
     }
     create()
     {
     // define objects
-    this.add.text(50, 50, 'start menu')
+    this.add.text(50, 50, 'start menu');
+    codey = this.add.sprite(30, 200, 'codey');
     }
     update()
     {
