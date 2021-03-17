2247672a256dddada2aa7540745970880351986a
RickM208 <78962706+RickM208@users.noreply.github.com>
2021-03-17 14:46:07 +0100

    menu

---
+++ Game/loadscene.js
@@ -0,0 +1,10 @@
+// /** @type {import("")} */
+// preload()
+// {
+//     //laden van plaatjes
+
+//     this.load.image("playBtn", "./assets/play.png");
+//     this.load.image("hndBtn", "./assets/handleiding.png");
+//     this.load.image("car", "./assets/car.png");
+
+// }
