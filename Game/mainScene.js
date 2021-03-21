130917500dd461f65fe9d8b77fb8ae26b81c9452
JoranFloor <80403870+JoranFloor@users.noreply.github.com>
2021-03-21 02:15:46 +0100

    Update mainScene.js

--- Game/mainScene.js
+++ Game/mainScene.js
@@ -1,33 +1,8 @@
 var car;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 var car1;
 var cursors;
 var keys;
 var finish
-
-=======
-var tracker1;
-var tracker2;
-var cursors;
->>>>>>> parent of bf611d5 (Little Changes to some files and added some in preperation)
-=======
-var tracker1;
-var tracker2;
-var cursors;
->>>>>>> parent of bf611d5 (Little Changes to some files and added some in preperation)
-=======
-var tracker1;
-var tracker2;
-var cursors;
->>>>>>> parent of d2b04f0 (a)
-=======
-var tracker1;
-var tracker2;
-var cursors;
->>>>>>> parent of d2b04f0 (a)
 
 class mainScene extends Phaser.Scene {
     
@@ -58,42 +33,11 @@ class mainScene extends Phaser.Scene {
         car.setFrictionAir(0.2);
         car.setMass(10);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
         car1 = this.matter.add.sprite(450,275, 'car4').setScale(0.22);
         car1.setFrictionAir(0.2);
         car1.setMass(10);
         
-
-        //var points0 = [ // initialize point on road X and Y
-          //  450,600,550,600,650,600,750,550,750,450,750,350,750,
-           // 250,750,150,650,100,550,150,450,100,350,100,250,150,
-          //  250,250,350,300,450,250,500,350,600,250,650,300,650,
-          //  400,600,450,500,450,400,400,300,400,250,450,250,550,
-          //  350,600,450,600
-       // ];
-
-        // var points1 = [ // initialize point on road X and Y
-        //     450,600,550,600,650,600,750,550,750,450,730,345,758,
-        //     244,752,134,650,100,540,122,450,100,335,91,250,150,
-        //     250,250,350,300,440,240,504,344,600,250,658,258,662,
-        //     359,638,470,532,468,436,406,324,385,268,452,269,556,
-        //     350,600,450,600
-        // ];
-        var points0 = [448,270,550,250,650,250,750,250,750,150,850,
-            150,850,250,850,350,850,450,830,533,750,550,700,500,700,
-            400,600,400,583,499,548,564,444,560,350,550,250,550,117,
-            554,72,501,136,450,150,350,150,250,165,170,250,150,340,
-            167,350,250,447,270];
-
-        var points1 = [450,260,549,268,648,259,750,250,761,146,850,
-            150,850,250,850,350,850,450,817,530,724,526,700,450,629,
-            382,571,438,554,564,456,573,354,541,250,550,150,544,72,
-            500,146,428,150,350,150,250,187,161,339,158,350,250,450,260];
-=======
         this.matter.world.setBounds(10, 50, 1000, 600);
-
 
         var points0 = [ // initialize point on road X and Y
             450,600,550,600,650,600,750,550,750,450,750,350,750,
@@ -102,49 +46,13 @@ class mainScene extends Phaser.Scene {
             400,600,450,500,450,400,400,300,400,250,450,250,550,
             350,600,450,600
         ];
-
-        // var points1 = [ // initialize point on road X and Y
-        //     450,600,550,600,650,600,750,550,750,450,730,345,758,
-        //     244,752,134,650,100,540,122,450,100,335,91,250,150,
-        //     250,250,350,300,440,240,504,344,600,250,658,258,662,
-        //     359,638,470,532,468,436,406,324,385,268,452,269,556,
-        //     350,600,450,600
-        // ];
->>>>>>> parent of d2b04f0 (a)
-
-=======
-        this.matter.world.setBounds(10, 50, 1000, 600);
-
-
-        var points0 = [ // initialize point on road X and Y
+        var points1 = [ // initialize point on road X and Y
             450,600,550,600,650,600,750,550,750,450,750,350,750,
             250,750,150,650,100,550,150,450,100,350,100,250,150,
             250,250,350,300,450,250,500,350,600,250,650,300,650,
             400,600,450,500,450,400,400,300,400,250,450,250,550,
             350,600,450,600
         ];
-
-        // var points1 = [ // initialize point on road X and Y
-        //     450,600,550,600,650,600,750,550,750,450,730,345,758,
-        //     244,752,134,650,100,540,122,450,100,335,91,250,150,
-        //     250,250,350,300,440,240,504,344,600,250,658,258,662,
-        //     359,638,470,532,468,436,406,324,385,268,452,269,556,
-        //     350,600,450,600
-        // ];
-
->>>>>>> parent of d2b04f0 (a)
-        var points1 = [447,270,447,270,563,273,666,266,732,268,
-            756,240,764,177,773,142,808,134,835,148,836,175,840,
-            215,841,256,839,288,837,331,840,366,841,417,838,455,
-            838,506,799,533,750,534,716,528,699,480,698,429,681,
-            399,622,393,581,405,573,441,569,488,568,522,546,575,
-            496,575,456,572,413,549,382,542,295,543,244,543,180,
-            542,112,543,80,521,85,489,105,473,129,464,156,442,
-            158,408,158,384,160,357,155,331,154,292,156,258,154,
-            226,156,193,180,165,208,165,241,162,268,163,308,163,
-            335,165,349,172,364,198,371,233,375,256,403,271,426,
-            270,447,270];
-
         // choose path to take
         var point = Phaser.Math.Between(0, 1);
 
@@ -159,31 +67,11 @@ class mainScene extends Phaser.Scene {
         // Create a racing car
         var lemming = this.add.follower(curve, 447,270, 'car1').setScale(0.6);
         // choose car speed
-<<<<<<< HEAD
-<<<<<<< HEAD
+
         var duration = Phaser.Math.Between(14000, 15000);
         var add = Phaser.Math.Between(0, 1500);
         var add1 = Phaser.Math.Between(0, 1500);
 
-=======
-        this.matter.world.setBounds(10, 50, 1000, 600);
-
-
-        var points0 = [ // initialize point on road X and Y
-            450,600,550,600,650,600,750,550,750,450,750,350,750,
-            250,750,150,650,100,550,150,450,100,350,100,250,150,
-            250,250,350,300,450,250,500,350,600,250,650,300,650,
-            400,600,450,500,450,400,400,300,400,250,450,250,550,
-            350,600,450,600
-        ];
-
-        // var points1 = [ // initialize point on road X and Y
-        //     450,600,550,600,650,600,750,550,750,450,730,345,758,
-        //     244,752,134,650,100,540,122,450,100,335,91,250,150,
-        //     250,250,350,300,440,240,504,344,600,250,658,258,662,
-        //     359,638,470,532,468,436,406,324,385,268,452,269,556,
-        //     350,600,450,600
-        // ];
 
         var points1 = [447,270,447,270,563,273,666,266,732,268,
             756,240,764,177,773,142,808,134,835,148,836,175,840,
@@ -212,18 +100,8 @@ class mainScene extends Phaser.Scene {
         var lemming = this.add.follower(curve, 447,270, 'car1').setScale(0.6);
         // choose car speed
         var duration = Phaser.Math.Between(12500, 14000);
-<<<<<<< HEAD
->>>>>>> parent of bf611d5 (Little Changes to some files and added some in preperation)
-=======
->>>>>>> parent of bf611d5 (Little Changes to some files and added some in preperation)
-=======
-        var duration = Phaser.Math.Between(12500, 14000);
->>>>>>> parent of d2b04f0 (a)
-=======
-        var duration = Phaser.Math.Between(12500, 14000);
->>>>>>> parent of d2b04f0 (a)
+
         // Let racing car follow path
-        
         console.log('race duration = ', duration)
         lemming.startFollow({
             duration: duration,
